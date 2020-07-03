a9b773d5f65e8794d688b7aed871d48b11affced
huang.shuai <huang.shuai@advantech.com.cn>
2020-07-03 16:26:46 +0800

    init

--- cns-k8s/src/main/java/com/ad/listener/TagMapListener.java
+++ cns-k8s/src/main/java/com/ad/listener/TagMapListener.java
@@ -12,7 +12,9 @@ import org.springframework.stereotype.Component;
 public class TagMapListener implements ApplicationListener<ContextRefreshedEvent> {
     @Override
     public void onApplicationEvent(ContextRefreshedEvent event) {
+        //将设备类型和taglist加载到内存中，为创建monitor做准备
         ExcelReaderUtil.parseTagConfigSheet();
+        //将设备大类和名字与设备类型信息加载到内存中，并创建profile
         ExcelReaderUtil.parseDeviceConfigSheet();
     }
 }
