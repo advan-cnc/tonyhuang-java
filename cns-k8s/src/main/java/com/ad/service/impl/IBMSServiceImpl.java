a1a6fd58e5cdc3e31926001b4a27a162961d8d44
huang.shuai <huang.shuai@advantech.com.cn>
2020-07-03 14:18:02 +0800

    init

--- cns-k8s/src/main/java/com/ad/service/impl/IBMSServiceImpl.java
+++ cns-k8s/src/main/java/com/ad/service/impl/IBMSServiceImpl.java
@@ -27,7 +27,6 @@ import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
-import static com.ad.util.ExcelReaderUtil.parseTagConfigSheet;
 
 @Service
 public class IBMSServiceImpl implements IBMSService {
